5253545ee162882cd09a9cff2b797df7a5313d87
Safikul Islam <islam.safikul@hotmail.com>
2023-02-19 20:07:08 +0530

    Initial

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## For Proxy Configuration
+
+https://angular.io/guide/build#proxying-to-a-backend-server
+
+## Box Shadow
+
+https://getcssscan.com/css-box-shadow-examples
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './modules/unauth/login/login.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { HotToastModule } from '@ngneat/hot-toast';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    HotToastModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
